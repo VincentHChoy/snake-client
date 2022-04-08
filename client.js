2e502ad9fe216f2c4e17feae7cdac02497327b0f
VincentHChoy <vhchoy@ualberta.ca>
2022-04-08 18:19:49 +0000

    eslint all files

--- client.js
+++ client.js
@@ -1,24 +1,16 @@
+const { IP, PORT } = require("./constants");
+console.log(IP);
 // establishes a connection with the game server
 const net = require("net");
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: '50541',
+    host: IP,
+    port: PORT
   });
 
   conn.on("connect", () => {
-    console.log("Successfully connected to game server")
+    console.log("Successfully connected to game server");
   });
-
-  // conn.on("connect", () => {
-  //   // setInterval(()=>{
-  //   // console.log("Move: up")
-  //   // conn.write("Move: up");
-  //   // console.log("Move: left")
-  //   // conn.write("Move: left")
-  //   // },500)
-
-  // });
   
   conn.on('connect', () => {
     conn.write('Name: VC');
@@ -28,11 +20,11 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on("data", (data) => {
-    console.log('data: ', data)
+    console.log('data: ', data);
   });
 
 
   return conn;
 };
 
-module.exports = {connect}
+module.exports = {connect};
--- input.js
+++ input.js
@@ -1,37 +1,35 @@
-// const {connect} = require('./client')
-
-let connection
-const setupInput = function (conn) {
+let connection;
+const setupInput = function(conn) {
   const stdin = process.stdin;
-  stdin.setRawMode(true)
-  stdin.setEncoding("utf8")
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
-  connection = conn
+  connection = conn;
   return stdin;
-}
+};
 
-const handleUserInput = function (key) {
+const handleUserInput = function(key) {
 
   if (key === '\u0003') { //ctrl c exits code
     process.exit();
   }
   if (key === `w`) {
-    console.log("Move: up")
+    console.log("Move: up");
     connection.write("Move: up");
   }
   if (key === `s`) {
-    console.log("Move: down")
+    console.log("Move: down");
     connection.write("Move: down");
 
   }
   if (key === `a`) {
-    console.log("Move: left")
+    console.log("Move: left");
     connection.write("Move: left");
 
   }
   if (key === `d`) {
-    console.log("Move: right")
+    console.log("Move: right");
     connection.write("Move: right");
 
   }
@@ -45,13 +43,6 @@ const handleUserInput = function (key) {
     connection.write("Say: XD");
 
   }
-  // your code here
 };
-// console.log("Move: left")
 
-// conn.write("Move: up");
-// conn.write("Move: left")
-
-
-
-module.exports = { setupInput }
+module.exports = { setupInput };
