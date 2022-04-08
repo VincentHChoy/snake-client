44a6ebae2b0b9548c433384913bb6aa75db8a122
VincentHChoy <vhchoy@ualberta.ca>
2022-04-08 16:34:52 +0000

    added setupInput function in client.js

--- client.js
+++ client.js
@@ -41,4 +41,22 @@ const connect = function () {
   return conn;
 };
 
+const setupInput = function (){
+  const stdin = process.stdin;
+  stdin.setRawMode(true)
+  stdin.setEncoding("utf8")
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+}
+
+const handleUserInput = function () {
+
+  if (key === '\u0003') { //ctrl c exits code
+    process.exit();
+  }
+  // your code here
+};
+
+
 module.exports = {connect}
